00c2f231f4f7d3c496a517c6e6b2fd5b32cee4dd
donghyun <gusdldi300@gmail.com>
2022-03-07 11:36:54 +0900

    Update S1931.java

--- S1931.java
+++ S1931.java
@@ -1,12 +1,10 @@
 package algorithm.greedy;
 
-//import java.util.Scanner;
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class S1931 {
     public static void testCode(String[] args) throws IOException {
@@ -15,10 +13,7 @@ public class S1931 {
     }
 
     private static int getMostRooms() throws IOException {
-        //Scanner scanner = new Scanner(System.in);
-        //int countRoom = scanner.nextInt();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int countRoom = Integer.parseInt(br.readLine());
 
         assert (countRoom >= 1 && countRoom <= 100000);
@@ -26,20 +21,35 @@ public class S1931 {
         MeetingRoom[] meetingRooms = new MeetingRoom[countRoom];
 
         for (int index = 0; index < countRoom; ++index) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int startTime = Integer.parseInt(st.nextToken());
-            int endTime = Integer.parseInt(st.nextToken());
+            String[] time = br.readLine().split(" ");
+            
+            long startTime = Long.parseLong(time[0]);
+            long endTime = Long.parseLong(time[1]);
 
             meetingRooms[index] = new MeetingRoom(startTime, endTime);
         }
 
-        quickSortAscending(meetingRooms);
+        if (countRoom == 1) {
+            return 1;
+        }
+
+        Arrays.sort(meetingRooms, new Comparator<MeetingRoom>() {
+            @Override
+            public int compare(MeetingRoom o1, MeetingRoom o2) {
+                int result = (int) (o1.getEndTime() - o2.getEndTime());
+                if (result == 0) {
+                    result = (int) (o1.getStartTime() - o2.getStartTime());
+                }
+
+                return result;
+            }
+        });
+        printMeetingRooms(meetingRooms);
 
         int countMostRoom = 1; // the first room is always inserted
         MeetingRoom lastRoom = meetingRooms[0];
         for (int index = 1; index < countRoom; ++index) {
-            if (lastRoom.getEndTime() <= meetingRooms[index].getStartTime()) {
+            if (meetingRooms[index].getStartTime() >= lastRoom.getEndTime()) {
                 countMostRoom++;
 
                 lastRoom = meetingRooms[index];
@@ -49,58 +59,28 @@ public class S1931 {
         return countMostRoom;
     }
 
-    private static void quickSortAscending(final MeetingRoom[] meetingRooms) {
-        quickSortAscendingRecursive(0, meetingRooms.length - 1, meetingRooms);
+    private static void printMeetingRooms(final MeetingRoom[] meetingRooms) {
+        for (int index = 0; index < meetingRooms.length; ++index) {
+            System.out.format("%d: (%d, %d)%s", index + 1, meetingRooms[index].getStartTime(), meetingRooms[index].getEndTime(), System.lineSeparator());
+        }
     }
 
-    private static void quickSortAscendingRecursive(int leftIndex, int rightIndex, final MeetingRoom[] meetingRooms) {
-        if (leftIndex >= rightIndex) {
-            return;
+    private static class MeetingRoom {
+        private long startTime;
+        private long endTime;
+
+        public MeetingRoom(final long startTime, final long endTime) {
+            this.startTime = startTime;
+            this.endTime = endTime;
         }
 
-        int completeIndex = partition(leftIndex, rightIndex, meetingRooms);
-        quickSortAscendingRecursive(leftIndex, completeIndex - 1, meetingRooms);
-        quickSortAscendingRecursive(completeIndex + 1, rightIndex, meetingRooms);
-    }
-
-    private static int partition(int leftIndex, int rightIndex, final MeetingRoom[] meetingRooms) {
-        int pivot = meetingRooms[rightIndex].getEndTime();
-
-        int swapIndex = leftIndex;
-        for (int index = leftIndex; index < rightIndex; ++index) {
-            if (meetingRooms[index].getEndTime() < pivot) {
-                swap(swapIndex, index, meetingRooms);
-
-                ++swapIndex;
-            }
+        public long getStartTime() {
+            return this.startTime;
         }
 
-        swap(swapIndex, rightIndex, meetingRooms);
-
-        return swapIndex;
+        public long getEndTime() {
+            return this.endTime;
+        }
     }
 
-    private static void swap(final int leftIndex, final int rightIndex, final MeetingRoom[] meetingRooms) {
-        MeetingRoom tempRoom = meetingRooms[leftIndex];
-        meetingRooms[leftIndex] = meetingRooms[rightIndex];
-        meetingRooms[rightIndex] = tempRoom;
-    }
-}
-
-class MeetingRoom {
-    private int startTime;
-    private int endTime;
-
-    public MeetingRoom(final int startTime, final int endTime) {
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    public int getStartTime() {
-        return this.startTime;
-    }
-
-    public int getEndTime() {
-        return this.endTime;
-    }
 }
