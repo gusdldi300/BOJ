a848a3dd2980f58973433d2a1fce0917de6597a4
donghyun <gusdldi300@gmail.com>
2024-02-13 14:51:30 +0900

    updated

--- G15661.java
+++ G15661.java
@@ -3,11 +3,63 @@ package backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class G15661 {
+    private static int sPointsSize;
+    private static int[] sSumRow;
+    private static int[] sSumCol;
+
+    private static int sMinDiff = Integer.MAX_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        sPointsSize = Integer.parseInt(br.readLine());
+        assert (sPointsSize >= 4 && sPointsSize <= 20);
+
+        sSumRow = new int[sPointsSize];
+        sSumCol = new int[sPointsSize];
+
+        int sum = 0;
+        for (int row = 0; row < sPointsSize; ++row) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int col = 0; col < sPointsSize; ++col) {
+                int point = Integer.parseInt(st.nextToken());
+
+                sSumRow[row] += point;
+                sSumCol[col] += point;
+
+                sum += point;
+            }
+        }
+
+        getMinDifferenceRecursive(0, 0, sum);
+
+        System.out.print(sMinDiff);
+        br.close();
+    }
+
+    private static void getMinDifferenceRecursive(int index, int count, int sum) {
+        if (index == sPointsSize || count == (sPointsSize / 2)) {
+            sMinDiff = Math.min(sMinDiff, Math.abs(sum));
+
+            return;
+        }
+
+        getMinDifferenceRecursive(index + 1, count + 1, sum - sSumRow[index] - sSumCol[index]);
+        getMinDifferenceRecursive(index + 1, count, sum);
+    }
+
+    /*
     private static int sArraySize;
     private static int[][] sArray;
+
+    private static int sMinDiff = Integer.MAX_VALUE;
+    private static boolean[] sPicked;
+
+    private static HashMap<String, Integer> cache = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,9 +76,7 @@ public class G15661 {
         }
 
         sPicked = new boolean[sArraySize];
-
-        int totalCount = (sArraySize % 2) == 0 ? (sArraySize / 2) : (sArraySize / 2) + 1;
-        for (int count = 1; count <= totalCount; ++count) {
+        for (int count = 1; count <= (sArraySize / 2); ++count) {
             int[] startTeam = new int[count];
             int[] linkTeam = new int[sArraySize - count];
 
@@ -35,9 +85,6 @@ public class G15661 {
 
         System.out.print(sMinDiff);
     }
-
-    private static int sMinDiff = Integer.MAX_VALUE;
-    private static boolean[] sPicked;
 
     private static void getMinDifferenceRecursive(int nextPlayer, int count, int totalCount, final int[] startTeam, final int[] linkTeam) {
         if (count == totalCount) {
@@ -75,4 +122,6 @@ public class G15661 {
 
         return sum;
     }
+
+    */
 }
