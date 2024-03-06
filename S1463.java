b77b217c7e7bd3cfd7f3f6dbb992c2db11c9db2b
donghyun <gusdldi300@gmail.com>
2024-03-07 03:51:44 +0900

    updated

--- S1463.java
+++ S1463.java
@@ -1,85 +1,36 @@
-package algorithm.DP;
+package dp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class S1463 {
-    public static void testCode(String[] args) throws IOException {
-        int count = getLeastComputation();
-        System.out.println(count);
-    }
+    private static int MAX_NUMBER = 1000000;
+    private static int sNumber;
 
-    public static int getLeastComputation() throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int number = Integer.parseInt(bf.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sNumber = Integer.parseInt(br.readLine());
 
-        if (number == 1) {
-            return 0;
-        }
+        // 테이블 정의: D[i], i를 1로 만들기 위해 필요한 연산 사용 횟수의 최솟값
+        // 점화식: D[k] = min(D[k / 3] + 1, D[k / 2] + 1, D[k - 1] + 1)
+        // 초기값 정의: D[1] = 0
 
-        if (number == 2 || number == 3) {
-            return 1;
-        }
-
-        int[] cache = new int[number + 1];
-        cache[2] = 1;
-        cache[3] = 1;
-
-        // Put least calculation count in the cell
-        for (int index = 4; index <= number; ++index) {
-            if ((index % 3) == 0) {
-                int count = cache[index / 3];
-                if ((index % 2) == 0) {
-                    count = Math.min(count, cache[index / 2]);
-                }
-
-                cache[index] = 1 + Math.min(count, cache[index - 1]);
-            } else if ((index % 2) == 0) {
-                cache[index] = 1 + Math.min(cache[index / 2], cache[index - 1]);
-            } else {
-                cache[index] = 1 + cache[index - 1];
+        int[] minCalCounts = new int[MAX_NUMBER + 1];
+        minCalCounts[1] = 0;
+        for (int num = 2; num <= sNumber; ++num) {
+            int lastMin = minCalCounts[num - 1];
+            if (num % 3 == 0) {
+                lastMin = Math.min(lastMin, minCalCounts[num / 3]);
             }
+
+            if (num % 2 == 0) {
+                lastMin = Math.min(lastMin, minCalCounts[num / 2]);
+            }
+
+            minCalCounts[num] = lastMin + 1;
         }
 
-        return cache[number];
+        System.out.print(minCalCounts[sNumber]);
     }
 }
-
-
-//    public static int getLeastComputation() throws IOException {
-//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//
-//        int number = Integer.parseInt(bf.readLine());
-//
-//        int count = 0;
-//
-//        int temp = number;
-//
-//
-//        while (number != 1) {
-//            System.out.println(number);
-//
-//            temp = number % 3;
-//
-//            if (temp == 0) {
-//                number /= 3;
-//            } else if (temp == 1) {
-//                if (((number / 2) % 2) == 0) {
-//                    number /= 2;
-//                } else {
-//                    number -= 1;
-//                }
-//            } else {
-//                if ((number % 2) == 0) {
-//                    number /= 2;
-//                } else {
-//                    number -= 1;
-//                }
-//            }
-//
-//            ++count;
-//        }
-//
-//        return count;
-//    }
