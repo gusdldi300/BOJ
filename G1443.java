1e2ec7b43b755153ed208a46d112e8e61b54f4bb
donghyun <gusdldi300@gmail.com>
2024-03-13 04:02:12 +0900

    solved

--- G1443.java
+++ G1443.java
@@ -3,6 +3,7 @@ package backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class G1443 {
@@ -13,6 +14,7 @@ public class G1443 {
     private static int sMultipleCount;
 
     private static int sMaxDisplayNumber;
+    private static int sBiggestNumber = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,30 +29,29 @@ public class G1443 {
         }
 
         sMaxDisplayNumber = ((int) Math.pow(10, sDisplayDigit)) - 1;
-        displayBiggestNumberRecursive(0, 1);
+        displayBiggestNumberRecursive(0, START_NUMBER, 1);
 
-        if (sBiggestNumber == Integer.MAX_VALUE) {
+        if (sBiggestNumber == Integer.MIN_VALUE) {
             sBiggestNumber = -1;
         }
 
         System.out.print(sBiggestNumber);
     }
 
-    private static int sBiggestNumber = Integer.MIN_VALUE;
 
-    private static void displayBiggestNumberRecursive(int count, int number) {
+    private static void displayBiggestNumberRecursive(int count, int nextNumber, int displayNumber) {
         if (count == sMultipleCount) {
-            sBiggestNumber = Math.max(sBiggestNumber, number);
+            sBiggestNumber = Math.max(sBiggestNumber, displayNumber);
             return;
         }
 
-        for (int i = END_NUMBER; i >= START_NUMBER; --i) {
-            int newNumber = number * i;
-            if (newNumber > sMaxDisplayNumber) {
+        for (int i = nextNumber; i <= END_NUMBER; ++i) {
+            int newDisplayNumber = displayNumber * i;
+            if (newDisplayNumber > sMaxDisplayNumber) {
                 return;
             }
 
-            displayBiggestNumberRecursive(count + 1, newNumber);
+            displayBiggestNumberRecursive(count + 1, i, newDisplayNumber);
         }
     }
 
