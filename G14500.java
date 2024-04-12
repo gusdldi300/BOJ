af80e116ae35eeea420f077e741b76b0fcdb0396
donghyun <gusdldi300@gmail.com>
2024-04-12 20:33:15 +0900

    updated

--- G14500.java
+++ G14500.java
@@ -3,7 +3,6 @@ package simulation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class G14500 {
     private static boolean[][][] sBlocks = new boolean[][][] {
@@ -119,6 +118,7 @@ public class G14500 {
             },
     };
 
+    /*
     private static int[][] sPaper;
     private static int sRowSize;
     private static int sColSize;
@@ -157,7 +157,6 @@ public class G14500 {
                         }
                     }
 
-
                     if (sum > maxSum) {
                         maxSum = sum;
                     }
@@ -166,5 +165,97 @@ public class G14500 {
         }
 
         System.out.print(maxSum);
+    }
+    */
+
+    private static final int MAX_COUNT = 4;
+    private static int[][] sPaper;
+    private static int sRowSize;
+    private static int sColSize;
+
+    private static int[] sRowDirs = new int[] {-1, 0, 1, 0};
+    private static int[] sColDirs = new int[] {0, 1, 0, -1};
+
+    private static int sMaxSum = Integer.MIN_VALUE;
+    private static boolean[][] sVisited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split(" ");
+        sRowSize = Integer.parseInt(inputs[0]);
+        sColSize = Integer.parseInt(inputs[1]);
+
+        sPaper = new int[sRowSize][sColSize];
+        sVisited = new boolean[sRowSize][sColSize];
+        for (int row = 0; row < sRowSize; ++row) {
+            inputs = br.readLine().split(" ");
+            for (int col = 0; col < sColSize; ++col) {
+                sPaper[row][col] = Integer.parseInt(inputs[col]);
+            }
+        }
+
+        for (int row = 0; row < sRowSize; ++row) {
+            for (int col = 0; col < sColSize; ++col) {
+                sVisited[row][col] = true;
+                getTetrominoMaxSumRecursive(row, col, 1, sPaper[row][col]);
+                sVisited[row][col] = false;
+
+                // ㅗ
+                outer:
+                for (int startIndex = 0; startIndex < sRowDirs.length; ++startIndex) {
+                    int sum = sPaper[row][col];
+                    for (int count = 0; count < MAX_COUNT - 1; ++count) {
+                        int dirIndex = (startIndex + count) % sRowDirs.length;
+
+                        int nextRow = row + sRowDirs[dirIndex];
+                        int nextCol = col + sColDirs[dirIndex];
+
+                        if (!isOnBoundary(nextRow, nextCol)) {
+                            continue outer;
+                        }
+
+                        sum += sPaper[nextRow][nextCol];
+                    }
+
+                    sMaxSum = Math.max(sMaxSum, sum);
+                }
+            }
+        }
+
+        System.out.print(sMaxSum);
+    }
+
+    private static boolean isOnBoundary(int row, int col) {
+        if (row < 0 || row >= sRowSize || col < 0 || col >= sColSize) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static void getTetrominoMaxSumRecursive(int row, int col, int count, int sum) {
+        if (count == MAX_COUNT) {
+            sMaxSum = Math.max(sMaxSum, sum);
+
+            return;
+        }
+
+        for (int dirIndex = 0; dirIndex < sRowDirs.length; ++dirIndex) {
+            int nextRow = row + sRowDirs[dirIndex];
+            int nextCol = col + sColDirs[dirIndex];
+
+            if (!isOnBoundary(nextRow, nextCol)) {
+                continue;
+            }
+
+            if (sVisited[nextRow][nextCol]) {
+                continue;
+            }
+
+            sVisited[nextRow][nextCol] = true;
+            getTetrominoMaxSumRecursive(nextRow, nextCol, count + 1, sum + sPaper[nextRow][nextCol]);
+            sVisited[nextRow][nextCol] = false;
+        }
     }
 }
